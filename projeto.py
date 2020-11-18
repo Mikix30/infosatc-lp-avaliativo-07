1ef51ea4f870d8e1eb9784c95729729513f92017
Júlia Barboza Brunelli <69260646+Mikix30@users.noreply.github.com>
2020-11-18 15:26:56 -0300

    Fix: Adiciona projeto.

--- projeto.py
+++ projeto.py
@@ -1,12 +1,12 @@
 from random import randint
 cpu = randint(1,6)
-print("vou pensar em um numero entre 1 e 6: ")
+print("Vou pensar em um número entre 1 e 6: ")
 acertou = False
 while not acertou:
-    jogador = int(input("qual numero eu pensei ? :"))
+    jogador = int(input("Qual número eu pensei ? :"))
     if jogador == cpu:
         acertou=True
-        print("acertou!!!")
+        print("Acertou!!!")
     else:
         if jogador > cpu:
             print("O número é um pouco menor.")
